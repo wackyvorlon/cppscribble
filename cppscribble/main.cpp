e3f723406d08dbb5325b415ccedfc3d63abe79c5
Paul Anderson <paul@andersonloco.com>
2011-11-12 23:24:51 -0500

    Initial Commit

---
+++ cppscribble/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  cppscribble
+//
+//  Created by Paul Anderson on 11-11-12.
+//  Copyright (c) 2011 Anderson Locomotive Works. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
